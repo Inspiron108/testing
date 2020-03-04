6757230cbb212196b4cc57d4efb7075f12264776
Inspiron108 <54252008+Inspiron108@users.noreply.github.com>
2020-03-04 18:56:29 -0500

    new

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,4 @@
 #include "main.h"
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -23,12 +6,8 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
 }
-
 /**
  * Runs while the robot is in the disabled state of Field Management System or
  * the VEX Competition Switch, following either autonomous or opcontrol. When
@@ -45,8 +24,13 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	pros::Motor left_mtr(1);
+	pros::Motor right_mtr(2);
 
+
+}
 /**
  * Runs the user autonomous code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -58,7 +42,14 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+
+
+
+
+
+
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -74,19 +65,40 @@ void autonomous() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+pros::Motor L1_mtr(19);
+pros::Motor L2_mtr(20);
+pros::Motor R1_mtr(12);
+pros::Motor R2_mtr(11);
+pros::Motor Tray_mtr(5);
+
+
+int intakes = 0;
+
+
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
+		if(master.get_digital(DIGITAL_UP) == 1)
+		{
+
+
+		}
+
+
+
+
+		int s = master.get_analog(ANALOG_RIGHT_X);
+		int t = master.get_analog(ANALOG_LEFT_X);
+		int f = master.get_analog(ANALOG_RIGHT_Y);
+
+
+		R1_mtr = f + t - s;
+		R2_mtr = f - t + s;
+		L1_mtr = f + t + s;
+		L2_mtr = f + t - s;
+
+
 		pros::delay(20);
 	}
 }
