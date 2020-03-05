fa6b18acd4daae8cdba3bb19949094be0af7f022
Inspiron108 <54252008+Inspiron108@users.noreply.github.com>
2020-03-05 12:03:15 -0500

    This might work

--- src/main.cpp
+++ src/main.cpp
@@ -68,10 +68,12 @@ void opcontrol() {
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 pros::Motor L1(19);
 pros::Motor L2(20);
-pros::Motor R1(12);
-pros::Motor R2(11);
+pros::Motor R1(12,true);
+pros::Motor R2(11,true);
 pros::Motor Tray(5);
-
+pros::Motor I1(2);
+pros::Motor I2(3,true);
+pros::Motor Arm(6,true);
 
 int intakes = 0;
 
@@ -79,13 +81,61 @@ int intakes = 0;
 
 	while (true) {
 
-		if(master.get_digital(DIGITAL_UP) == 1)
-		{
-			Tray.move_voltage(12000);
-			
+//tray
+		if(master.get_digital(DIGITAL_UP) == 1){
+				Tray.move_voltage(12000);
+				I1.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+				I2.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			}
+			else
+			{
+				if(master.get_digital(DIGITAL_DOWN) == 1){
+					Tray.move_voltage(-12000);
+					I1.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+					I2.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+				}
+				else
+				{
+				Tray.move_velocity(0);
+				}
 		}
 
+//arm
+	if(master.get_digital(DIGITAL_L1) == 1)
+	{
+		Arm.move_voltage(12000);
+		Tray.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	}
+	else
+	{
+		if(master.get_digital(DIGITAL_L2) == 1){
+			Arm.move_voltage(-12000);
+			Tray.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		}
+		else
+		{
+		Arm.move_velocity(0);
+		}
+	}
 
+//Intakes
+	if(master.get_digital(DIGITAL_R1) == 1)
+	{
+		I1.move_voltage(12000);
+		I2.move_voltage(12000);
+	}
+	else
+	{
+		if(master.get_digital(DIGITAL_DOWN) == 1){
+			I1.move_voltage(-12000);
+			I2.move_voltage(12000);
+		}
+		else
+		{
+		I1.move_velocity(0);
+		I2.move_velocity(0);
+		}
+	}
 
 
 
@@ -95,12 +145,21 @@ int intakes = 0;
 		int f = master.get_analog(ANALOG_RIGHT_Y);
 
 
-		R1 = f + t - s;
-		R2 = f - t + s;
+		R1 = -f + t - s;
+		R2 = -f - t + s;
 		L1 = f + t + s;
 		L2 = f + t - s;
 
-
 		pros::delay(20);
+
+//Intake Breaking
+		if(I1.get_target_velocity() == 0)
+		{
+			I1.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+			I2.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+		}
+
+
+
 	}
 }
